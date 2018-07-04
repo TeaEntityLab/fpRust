a46ceb58e2b27f397ef27542beb744bc5e86754e
JunYi JohnTeee Lee <johnteee@gmail.com>
2018-07-04 21:58:47 +0800

    Remove wrong generic codes

--- src/monadio.rs
+++ src/monadio.rs
@@ -4,24 +4,22 @@ use std::panic;
 
 use common::Subscription;
 
-type F<Y> = FnOnce() -> Y;
+// type F<Y> = FnOnce() -> Y;
+//
+// fn _just<Y: 'static>(r :Y) -> Box<F<Y>> {
+//     return Box::new(|| r);
+// }
 
-fn _just<Y: 'static>(r :Y) -> Box<F<Y>> {
-    return Box::new(|| r);
-}
-
-pub struct MonadIO<F> {
+pub struct MonadIO<Y, F: FnOnce()->Y> {
     effect : F,
 }
 
-impl <F> MonadIO<F> {
+impl <Y, F: FnOnce()->Y> MonadIO<Y, F> {
 
-    // pub fn just<Y: 'static>(r :Y) -> MonadIO<F> {
-    //     return MonadIO {
-    //         effect: || r,
-    //     };
+    // pub fn just<Y2: 'static, F2: FnOnce()->Y2>(r :Y2) -> MonadIO<Y2, F2> {
+    //     return MonadIO::new(|| r);
     // }
-    pub fn new(effect: F) -> MonadIO<F> {
+    pub fn new(effect: F) -> MonadIO<Y, F> {
         return MonadIO {
             effect,
         }
