158de3e1dcc2d470590d6dad9a12fc1db0303cfc
JunYi JohnTeee Lee <johnteee@gmail.com>
2018-07-06 00:28:33 +0800

    partial Handler implements

--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,37 @@
+use std::thread;
 
 pub trait Subscription<X> {
     fn on_next(&mut self, x : X);
+}
+
+pub trait Handler {
+    fn post(&mut self, func: impl FnOnce());
+}
+
+pub struct HandlerThread {
+    cancel: bool,
+    terminated: bool,
+    th : &'static thread::Thread,
+}
+
+impl HandlerThread {
+    // fn new() -> HandlerThread {
+    //     let joinHandle = &thread::spawn(|| {
+    //
+    //     });
+    //     let thread = joinHandle.thread();
+    //
+    //     return HandlerThread {
+    //         cancel: false,
+    //         terminated: false,
+    //
+    //         th: &thread,
+    //     }
+    // }
+}
+
+impl Handler for HandlerThread {
+    fn post(&mut self, func: impl FnOnce()) {
+
+    }
 }
