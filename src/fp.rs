0a53ac6695876408a3fedf0f072308e4a70ed4b2
JunYi JohnTeee Lee <johnteee@gmail.com>
2018-07-15 11:50:10 +0800

    Add partial map reduce filter (reduce credit: https://github.com/dtolnay/reduce)

--- src/fp.rs
+++ src/fp.rs
@@ -2,19 +2,44 @@
 In this module there're implementations & tests
 of general functional programming features.
 */
-#[macro_export]
 
 /**
 Compose functions.
 
 *NOTE*: Credit https://stackoverflow.com/questions/45786955/how-to-compose-functions-in-rust
 */
+#[macro_export]
 macro_rules! compose {
     ( $last:expr ) => { $last };
     ( $head:expr, $($tail:expr), +) => {
         compose_two($head, compose!($($tail),+))
     };
 }
+#[macro_export]
+macro_rules! partial_vec {
+    ($func:expr, $second:expr) => {
+        |v| $func($second, v)
+    };
+}
+#[macro_export]
+macro_rules! map {
+    ($func:expr) => {
+        partial_vec!(map, $func)
+    };
+}
+#[macro_export]
+macro_rules! filter {
+    ($func:expr) => {
+        partial_vec!(filter, $func)
+    };
+}
+#[macro_export]
+macro_rules! reduce {
+    ($func:expr) => {
+        partial_vec!(reduce, $func)
+    };
+}
+
 
 /**
 Compose two functions into one.
@@ -33,6 +58,48 @@ where
     G: FnOnce(B) -> C,
 {
     move |x| g(f(x))
+}
+
+pub fn map<T, F, B>(f: impl FnMut(T) -> B, v: Vec<T>) -> Vec<B> {
+    v.into_iter().map(f).collect::<Vec<B>>()
+}
+
+pub fn filter<'r, T: 'r, F>(f: impl FnMut(&T) -> bool, v: Vec<T>) -> Vec<T> {
+    v.into_iter().filter(f).into_iter().collect::<Vec<T>>()
+}
+
+/**
+Implementations of `ECMASript`-like `reduce`()
+
+# Arguments
+
+* `T` - The generic type of data.
+
+*NOTE*: Credit https://github.com/dtolnay/reduce
+*/
+pub trait Reduce<T> {
+    fn reduce<F>(self, f: F) -> Option<T>
+    where
+        Self: Sized,
+        F: FnMut(T, T) -> T;
+}
+
+impl<T, I> Reduce<T> for I
+where
+    I: Iterator<Item = T>,
+{
+    #[inline]
+    fn reduce<F>(mut self, f: F) -> Option<T>
+    where
+        Self: Sized,
+        F: FnMut(T, T) -> T,
+    {
+        self.next().map(|first| self.fold(first, f))
+    }
+}
+
+pub fn reduce<'r, T: 'r, F>(f: impl FnMut(T, T) -> T, v: Vec<T>) -> Option<T> {
+    v.into_iter().reduce(f)
 }
 
 #[test]
