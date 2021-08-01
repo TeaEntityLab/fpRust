cf2fc5af94b3f1e7b5755191a219e1c73a34a3a9
John <johnteee@gmail.com>
2021-08-01 15:58:43 +0800

    fix publisher.subscribe_blocking_queue() strange implementations.

--- src/publisher.rs
+++ src/publisher.rs
@@ -94,20 +94,15 @@ where
         self.delete_observer(s);
     }
 
-    pub fn subscribe_blocking_queue(
-        &mut self,
-        queue: BlockingQueue<Arc<X>>,
-    ) -> BlockingQueue<Arc<X>> {
-        let mut queue_new: BlockingQueue<Arc<X>>;
-        {
-            queue_new = queue.clone();
-        }
+    pub fn subscribe_blocking_queue(&mut self, queue: &BlockingQueue<Arc<X>>) {
+        let mut queue_new = queue.clone();
         self.subscribe_fn(move |v| queue_new.put(v));
-
-        queue
     }
     pub fn as_blocking_queue(&mut self) -> BlockingQueue<Arc<X>> {
-        self.subscribe_blocking_queue(BlockingQueue::new())
+        let queue = BlockingQueue::new();
+        self.subscribe_blocking_queue(&queue);
+
+        queue
     }
 }
 
